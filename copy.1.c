76c89f407f512529ea9e46503cd14959f4eaf3f0
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-05-18 00:46:44 +0000

    whodunit code changing the colors of the image - it works

--- copy.1.c
+++ copy.1.c
@@ -74,16 +74,6 @@ int main(int argc, char *argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-
-            if (triple.rgbtRed == 0xff && triple.rgbtBlue == 0xff && triple.rgbtGreen == 0xff)
-            {
-                triple.rgbtRed = 0x00;
-                triple.rgbtGreen = 0x00;
-                triple.rgbtBlue = 0x00;
-            }
-
-
-
             // write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
         }
